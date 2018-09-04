d174b4d96f82e85159e6a52839d4635d9610c199
Bernardo Generozo <bernardo.generozo@gmail.com>
2018-09-03 22:04:16 -0300

    Adicionado leitura de arquivo xlsx para controller

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Routing\Controller as BaseController;
 use Rap2hpoutre\FastExcel\FastExcel;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\View;
 
 class StoreController extends BaseController
 {
     public function index()
     {
+        $products = (new FastExcel)->import('/var/www/lista.xlsx');
+
+        return View::make('store.index')
+            ->withProducts($products);
     }
 }
