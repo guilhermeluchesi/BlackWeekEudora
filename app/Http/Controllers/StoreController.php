f58b59f8f089498bdf6a8b9611fa2e9bc086f90a
Bernardo Generozo <bernardo.generozo@gmail.com>
2018-09-03 21:52:53 -0300

    Adicionado laravel ao projeto

---
+++ app/Http/Controllers/StoreController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Routing\Controller as BaseController;
+use Rap2hpoutre\FastExcel\FastExcel;
+
+class StoreController extends BaseController
+{
+    public function index()
+    {
+    }
+}
