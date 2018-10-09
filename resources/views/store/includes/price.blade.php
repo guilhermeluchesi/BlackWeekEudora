5c0598ba74697597b6700a5447c61880f3ed4ec3
Bernardo Ribeiro Generozo <bernardo.generozo@gmail.com>
2018-10-09 01:41:43 -0300

    Corrigido slider desk

--- resources/views/store/includes/price.blade.php
+++ resources/views/store/includes/price.blade.php
@@ -2,9 +2,16 @@
     <h2 class="txt-gold col-12">Faixa de preço</h2>
     <form class="col-12" action="/" id="sliderForm">
         <div class="input-group">
-            <input name="slider" type="range" class="custom-range" min="1" max="{!!$maxValue!!}" id="rating" onmousemove="evalSlider()" onchange="this.form.submit()">
+            <input name="slider" type="range" class="custom-range" value="{{Request::input('slider')}}" min="1" max="{!!$maxValue!!}" id="rating" onmousemove="evalSlider()" onchange="this.form.submit()">
         </div>
-        <output id="sliderVal">R$ 100</output>
+        @php
+            $value = 100;
+            if (Request::input('slider')) {
+                $value = Request::input('slider');
+            }
+
+        @endphp
+        <output id="sliderVal">R$ {!!$value!!}</output>
         <span class="right"><font color="black"> R$ {!!$maxValue!!}</font></span>
     </form>
 </div>
